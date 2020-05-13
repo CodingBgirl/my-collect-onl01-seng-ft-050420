3f3ba9b8f85e88ea99a0c3be084249a99d254ae3
Ibiene LongJohn <bgirlapollo@gmail.com>
2020-05-13 20:03:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   index = 0
   new_array = []
   while index < array.length
-    new_array << yield (array[index])
+    new_array << yield(array[index])
     index += 1
   end
   new_array
